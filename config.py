438083d7b37ebe122fed257850c854306fbcf36e
SmileyHackeer <aimefx813@gmail.com>
2024-07-25 18:47:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "22593897"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "375f4e7c559474de40b78ba61c2c2eb6" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7483202121:AAErbIHa6rfVLlwD4ZsAr3ShyEUiBdKRkdY" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
